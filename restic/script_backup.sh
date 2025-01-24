ea941ae4d795be3325464dd541a96f057fdeb922
alexislafrance <alexislafrance42@gmail.com>
2025-01-24 21:09:23 +0000

    add backup cloud with restic

---
+++ restic/script_backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source /home/alexis/Documents/.env
+
+export B2_ACCOUNT_ID
+export B2_ACCOUNT_KEY
+export RESTIC_PASSWORD
+
+
+sudo -E restic -r $RESTIC_REPOSITORY backup /mnt/md127 --exclude-file $EXCLUDES_FILE
+
+sudo -E restic -r $RESTIC_REPOSITORY forget --keep-daily 7 --keep-weekly 4 --keep-monthly 6 --prune
+sudo -E restic -r $RESTIC_REPOSITORY check
+
